3912eae8e1618aeee85594309c9e487bd2130b47
delmont <pazxx219@gmail.com>
2018-06-02 19:01:22 +0900

    エントリーファイル名変更、コミット漏れ修正、visualgdb関連をイグノア設定

---
+++ main/inc/sys.h
@@ -0,0 +1,17 @@
+#pragma once
+/* ファイル名:   sys.h                            */
+/* 説明:         システム全体共有ヘッダーファイル */
+/* インクルードファイル */
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+/* マクロ定義 */
+
+/* 基本型定義 */
+typedef uint8_t  U1;
+typedef uint16_t U2;
+typedef uint32_t U4;
+typedef uint64_t U8;
+
+/* プロトタイプ宣言 */
---
+++ main/vfd/Source.c
@@ -0,0 +1,6 @@
+#include "sys.h"
+
+U1 test_func()
+{
+   return (U1)1;
+}
